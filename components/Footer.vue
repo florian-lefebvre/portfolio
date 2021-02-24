c612a9df916433d1c464e725a9752c09e5534af1
Florian LEFEBVRE <florian.lefebvre3184@orange.fr>
2021-02-24 16:24:39 +0100

    fix broken badge and add modern generation

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,7 @@
     class="mt-6 text-gray-700 bg-gray-300 dark:bg-gray-700 dark:text-gray-300"
   >
     <div
+      ref="container"
       class="container p-4 mx-auto space-y-2 text-sm font-medium tracking-wider text-center"
     >
       <div>&copy; Florian LEFEBVRE {{ new Date().getFullYear() }}</div>
@@ -13,16 +14,22 @@
           'wcb-d': $colorMode.value === 'dark'
         }"
       ></div>
-      <script
-        src="https://unpkg.com/website-carbon-badges@1.1.1/b.min.js"
-        defer
-      ></script>
     </div>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    let badgeScript = document.createElement("script");
+    badgeScript.setAttribute(
+      "src",
+      "https://unpkg.com/website-carbon-badges@1.1.1/b.min.js"
+    );
+    badgeScript.setAttribute("defer", true);
+    this.$refs.container.appendChild(badgeScript);
+  }
+};
 </script>
 
 <style></style>
