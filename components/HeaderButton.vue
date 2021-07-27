485835b2b69eedac346adfd68dff1681cd1a576f
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-07-27 20:11:37 +0200

    improve header color

--- components/HeaderButton.vue
+++ components/HeaderButton.vue
@@ -6,7 +6,7 @@
     :rel="external ? 'noopener noreferrer' : null"
     @click="!external ? $emit('click') : null"
     :aria-label="label"
-    class="hover:bg-gray-200 dark:hover:bg-gray-600 rounded-full p-2 transition-colors duration-150 ease-in-out focus:outline-none"
+    class="hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full p-2 transition-colors duration-150 ease-in-out focus:outline-none"
   >
     <slot />
   </component>
