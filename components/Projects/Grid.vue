7fda2a7be31949d64f05d6912847da957b2f9ed2
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-03-22 19:22:51 +0100

    add projects tags filtering

--- components/Projects/Grid.vue
+++ components/Projects/Grid.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-    <div
-      v-for="(project, i) in projects"
-      v-motion
-      :initial="{
-        opacity: 0,
-        y: 20,
-      }"
-      :visible="{
-        opacity: 1,
-        y: 0,
-        transition: {
-          duration: 500,
-          delay: i * 50,
-        },
-      }"
-    >
+    <div v-for="(project, i) in projects">
       <ProjectsCard :project="project" detailed />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import projects from "~/data/projects";
+import { Project } from "~/types";
+
+defineProps<{ projects: Project[] }>();
 </script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -59,7 +59,30 @@
     </Container>
     <div class="bg-gray-900/50">
       <Container class="py-16 md:py-20">
-        <ProjectsGrid />
+        <div class="relative rounded-lg">
+          <div
+            class="absolute top-0 left-0 bottom-0 z-20 w-4 bg-gradient-to-r from-gray-900/75 to-gray-900/0"
+          ></div>
+          <div
+            class="absolute top-0 right-0 bottom-0 z-20 w-4 bg-gradient-to-l from-gray-900/75 to-gray-900/0"
+          ></div>
+          <div class="relative mb-8 flex space-x-2 overflow-x-scroll">
+            <button
+              type="button"
+              v-for="tag in ['All', ...tags]"
+              class="mb-2 flex-shrink-0 select-none rounded-md px-4 py-2 transition-colors"
+              :class="
+                selectedTag === tag
+                  ? 'bg-gradient font-semibold text-white'
+                  : 'bg-gray-800 text-gray-400 hover:bg-gray-700'
+              "
+              @click="selectedTag = tag"
+            >
+              {{ tag }}
+            </button>
+          </div>
+        </div>
+        <ProjectsGrid :projects="filteredProjects" />
       </Container>
     </div>
   </div>
@@ -70,6 +93,22 @@ import projects, { featured } from "~/data/projects";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
+
+const tags = [
+  ...new Set(projects.map((project) => project.global.technologies).flat()),
+].sort();
+
+const selectedTag = ref<string>("All");
+
+const filteredProjects = computed(() =>
+  projects.filter((project) => {
+    if (selectedTag.value === "All") {
+      return true;
+    }
+
+    return project.global.technologies.includes(selectedTag.value);
+  })
+);
 
 useSeo({
   title: t("pages.projects.seo.title"),
