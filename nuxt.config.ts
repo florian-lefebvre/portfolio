abc16fd27b150b36fde487e300c89397da4420ee
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-03-26 17:27:06 +0100

    defer fonts loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap",
+        defer: true,
       },
       { rel: "preconnect", href: "https://res.cloudinary.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com" },
