35e34dda4e03eca7016ec998a0cbf2111d9b461b
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-12-01 14:01:17 +0100

    improve header style

---
+++ src/hooks/useWindowEventListener.ts
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export default function (
+  event: string,
+  listener: () => void,
+  opts?: AddEventListenerOptions
+) {
+  useEffect(() => {
+    window.addEventListener(event, listener, opts);
+    listener();
+    return () => {
+      window.removeEventListener(event, listener);
+    };
+  });
+}
