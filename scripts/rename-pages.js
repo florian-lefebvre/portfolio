e792739028196b37cbd56e4a90bc4f964e2cd6ee
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-12-12 21:16:00 +0100

    update rename script

--- scripts/rename-pages.js
+++ scripts/rename-pages.js
@@ -3,6 +3,12 @@ import path from 'path'
 
 async function main() {
     async function rename(oldPath, newPath) {
+        try {
+            await fsp.access(oldPath)
+        } catch (e) {
+            console.warn(`Can't rename ${oldPath}`)
+            return
+        }
         await fsp.rename(oldPath, newPath)
     }
     const __dirname = path.resolve()
