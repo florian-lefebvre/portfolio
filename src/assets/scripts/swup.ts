48a4b8b57d370b7bd656550577cf26296ef216ce
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-12-13 13:23:26 +0100

    add page transitions

---
+++ src/assets/scripts/swup.ts
@@ -0,0 +1,52 @@
+// @ts-ignore
+import Swup from 'swup'
+// @ts-ignore
+import SwupA11yPlugin from '@swup/a11y-plugin'
+// @ts-ignore
+import SwupHeadPlugin from '@swup/head-plugin'
+// @ts-ignore
+import SwupScrollPlugin from '@swup/scroll-plugin'
+// @ts-ignore
+import SwupScriptsPlugin from '@swup/scripts-plugin'
+// @ts-ignore
+import SwupFadeTheme from '@swup/fade-theme'
+
+const swup = new Swup({
+    plugins: [
+        new SwupA11yPlugin(),
+        new SwupHeadPlugin(),
+        new SwupScrollPlugin({
+            doScrollingRightAway: true,
+            animateScroll: !window.matchMedia(
+                '(prefers-reduced-motion: reduce)'
+            ),
+        }),
+        new SwupScriptsPlugin(),
+        new SwupFadeTheme(),
+    ],
+})
+
+document.addEventListener('swup:samePage', (event) => {
+    history.replaceState(
+        '',
+        document.title,
+        window.location.pathname + window.location.search
+    )
+})
+
+document.addEventListener('swup:contentReplaced', (event) => {
+    const links: HTMLAnchorElement[] = Array.from(
+        document.querySelectorAll('header a')
+    )
+    for (const link of links) {
+        link.classList.remove('font-bold')
+        link.classList.add('font-medium')
+    }
+    const pathname =
+        window.location.pathname === '' ? '/' : window.location.pathname
+
+    links.shift()
+    const selectedLink = links.find((link) => link.pathname === pathname)
+    selectedLink?.classList.remove('font-medium')
+    selectedLink?.classList.add('font-bold')
+})
